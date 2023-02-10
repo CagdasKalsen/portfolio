e9c8336217b68224f4fde17f1e941597cb68a710
cagdaskalsen <cagdaskalsen@hotmail.com>
2023-02-10 01:01:51 -0500

    Icons replaced

--- portfolio/src/components/Sidebar/index.js
+++ portfolio/src/components/Sidebar/index.js
@@ -1,7 +1,7 @@
 import './index.scss'
 import { Link, NavLink } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 const Sidebar = () => {
   return (
     <>
@@ -10,15 +10,19 @@ const Sidebar = () => {
           <h2 className="first">Cagdas</h2>
           <h2 className="last">Kalsen</h2>
         </Link>
+
+        <nav>
+          <NavLink exact="true" activeclassname="active" to="/">
+            <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+          </NavLink>
+          <NavLink exact="true" activeclassname="active" to="/about">
+            <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+          </NavLink>
+          <NavLink exact="true" activeclassname="active" to="/contact">
+            <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+          </NavLink>
+        </nav>
       </div>
-      <nav>
-        <NavLink exact="true" activeclassname="active" to="/">
-          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-        </NavLink>
-        <NavLink exact="true" activeclassname="active" to="/">
-          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-        </NavLink>
-      </nav>
     </>
   )
 }
